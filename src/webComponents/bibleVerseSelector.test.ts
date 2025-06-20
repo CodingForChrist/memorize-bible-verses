186962a13801cad6044f983fcc936e2e801a8a12
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-06-19 23:35:26 -0500

    Set up vitest

---
+++ src/webComponents/bibleVerseSelector.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { BibleVerseSelector } from "./bibleVerseSelector";
+
+describe("<bible-verse-selector>", () => {
+  beforeEach(() => {
+    document.body.innerHTML = "";
+  });
+
+  test("should render form when selected-bible-id attribute is provided", () => {
+    document.body.innerHTML =
+      '<bible-verse-selector selected-bible-id="bba9f40183526463-01"></bible-verse-selector>';
+    const bibleVerseSelector = document.querySelector(
+      "bible-verse-selector",
+    ) as BibleVerseSelector;
+
+    expect(bibleVerseSelector.querySelector("#form-input-verse")).toBeTruthy();
+  });
+
+  test("should not render form when selected-bible-id attribute is missing", () => {
+    document.body.innerHTML = "<bible-verse-selector></bible-verse-selector>";
+    const bibleVerseSelector = document.querySelector(
+      "bible-verse-selector",
+    ) as BibleVerseSelector;
+
+    expect(bibleVerseSelector.innerHTML).toBe("");
+  });
+});
