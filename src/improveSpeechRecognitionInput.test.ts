7fba72a4abbc152b8543331711dea2b0ff2e1e30
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-07-12 08:12:24 -0500

    Improve transcript for book numbers

--- src/improveSpeechRecognitionInput.test.ts
+++ src/improveSpeechRecognitionInput.test.ts
@@ -1,13 +1,16 @@
 import { describe, expect, test } from "vitest";
 
-import { improveSpeechRecognitionInput } from "./improveSpeechRecognitionInput";
+import {
+  improveSpeechRecognitionInput,
+  parseVerseReferenceIntoParts,
+} from "./improveSpeechRecognitionInput";
 
 describe("improveSpeechRecognitionInput()", () => {
   test("should add missing colon between chapter and verse number", () => {
     const transcript =
-      "Genesis 11 In the beginning God created the heavens and the earth. Genesis 11";
+      "Genesis 11 In the beginning God created the heavens and the earth Genesis 11";
     const expectedOutput =
-      "Genesis 1:1 In the beginning God created the heavens and the earth. Genesis 1:1";
+      "Genesis 1:1 In the beginning God created the heavens and the earth Genesis 1:1";
 
     expect(
       improveSpeechRecognitionInput({
@@ -20,9 +23,9 @@ describe("improveSpeechRecognitionInput()", () => {
 
   test("should convert spelled out numbers to actual numbers", () => {
     const transcript =
-      "Genesis one one In the beginning God created the heavens and the earth. Genesis one one";
+      "Genesis one one In the beginning God created the heavens and the earth Genesis one one";
     const expectedOutput =
-      "Genesis 1:1 In the beginning God created the heavens and the earth. Genesis 1:1";
+      "Genesis 1:1 In the beginning God created the heavens and the earth Genesis 1:1";
 
     expect(
       improveSpeechRecognitionInput({
@@ -31,5 +34,58 @@ describe("improveSpeechRecognitionInput()", () => {
         verseText: expectedOutput,
       }),
     ).toBe(expectedOutput);
+  });
+
+  test("should convert ordinal numbers for books of the bible", () => {
+    const transcript =
+      "Second Corinthians 57 For we walk by faith not by sight second Corinthians 57";
+    const expectedOutput =
+      "2 Corinthians 5:7 For we walk by faith not by sight 2 Corinthians 5:7";
+
+    expect(
+      improveSpeechRecognitionInput({
+        transcript,
+        verseReference: "2 Corinthians 5:7",
+        verseText: expectedOutput,
+      }),
+    ).toBe(expectedOutput);
+  });
+});
+
+describe("parseVerseReferenceIntoParts()", () => {
+  test("parses a single verse", () => {
+    expect(parseVerseReferenceIntoParts("John 3:16")).toEqual({
+      bookName: "John",
+      bookNumber: undefined,
+      chapter: 3,
+      verseNumberStart: 16,
+      verseNumberEnd: 16,
+    });
+
+    expect(parseVerseReferenceIntoParts("2 Corinthians 5:17")).toEqual({
+      bookName: "Corinthians",
+      bookNumber: 2,
+      chapter: 5,
+      verseNumberStart: 17,
+      verseNumberEnd: 17,
+    });
+  });
+
+  test("parses a verse range", () => {
+    expect(parseVerseReferenceIntoParts("John 1:1-10")).toEqual({
+      bookName: "John",
+      bookNumber: undefined,
+      chapter: 1,
+      verseNumberStart: 1,
+      verseNumberEnd: 10,
+    });
+
+    expect(parseVerseReferenceIntoParts("2 Corinthians 5:1-17")).toEqual({
+      bookName: "Corinthians",
+      bookNumber: 2,
+      chapter: 5,
+      verseNumberStart: 1,
+      verseNumberEnd: 17,
+    });
   });
 });
--- src/improveSpeechRecognitionInput.ts
+++ src/improveSpeechRecognitionInput.ts
@@ -9,13 +9,13 @@ export function improveSpeechRecognitionInput({
   verseReference,
   verseText,
 }: ImproveSpeechRecognitionInputOptions) {
-  let improvedTranscript = addMissingColonsToBibleReference({
+  let improvedTranscript = replaceSpelledOutNumbersInBibleReference({
     transcript,
     verseReference,
     verseText,
   });
 
-  improvedTranscript = replaceSpelledOutNumbersInBibleReference({
+  improvedTranscript = addMissingColonsToBibleReference({
     transcript: improvedTranscript,
     verseReference,
     verseText,
@@ -44,35 +44,104 @@ function replaceSpelledOutNumbersInBibleReference({
   verseReference,
 }: ImproveSpeechRecognitionInputOptions) {
   // safari spells out numbers less than ten
-  const numberMap: Record<string, string> = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine",
+  const spelledOutNumbersMap: Record<string, string> = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
   };
-  const digits = verseReference.match(/\d+/g);
-  const book = verseReference.match(/[a-zA-Z]+/);
-  if (!book || !digits) {
-    return transcript;
-  }
 
-  const numbersSpelledOut = digits.map((digit) => {
-    return numberMap[digit] ?? digit;
-  });
+  // handle books like 1st and 2nd Corinthians
+  const ordinalNumbersMap: Record<string, string[]> = {
+    1: ["1st", "First", "first"],
+    2: ["2nd", "Second", "second"],
+    3: ["3rd", "Third", "third"],
+  };
 
-  const verseReferenceWithNumbersSpelledOut = `${book} ${numbersSpelledOut.join(" ")}`;
+  const { bookNumber, bookName, chapter, verseNumberStart } =
+    parseVerseReferenceIntoParts(verseReference);
+
   let improvedTranscript = transcript;
 
-  if (transcript.includes(verseReferenceWithNumbersSpelledOut)) {
-    improvedTranscript = improvedTranscript.replaceAll(
-      verseReferenceWithNumbersSpelledOut,
-      verseReference,
-    );
+  if (bookNumber && ordinalNumbersMap[bookNumber]) {
+    const bookNumberAsOrdinalNumbers = ordinalNumbersMap[bookNumber];
+
+    bookNumberAsOrdinalNumbers.forEach((bookNumberAsOrdinalNumber) => {
+      if (
+        improvedTranscript.includes(`${bookNumberAsOrdinalNumber} ${bookName}`)
+      ) {
+        improvedTranscript = improvedTranscript.replaceAll(
+          `${bookNumberAsOrdinalNumber} ${bookName}`,
+          `${bookNumber} ${bookName}`,
+        );
+      }
+    });
   }
+
+  if (spelledOutNumbersMap[chapter]) {
+    const chapterSpelledOut = spelledOutNumbersMap[chapter];
+    if (improvedTranscript.includes(`${bookName} ${chapterSpelledOut}`)) {
+      improvedTranscript = improvedTranscript.replaceAll(
+        `${bookName} ${chapterSpelledOut}`,
+        `${bookName} ${chapter}`,
+      );
+    }
+  }
+
+  if (spelledOutNumbersMap[verseNumberStart]) {
+    const verseNumberStartSpelledOut = spelledOutNumbersMap[verseNumberStart];
+    if (
+      improvedTranscript.includes(
+        `${bookName} ${chapter} ${verseNumberStartSpelledOut}`,
+      )
+    ) {
+      improvedTranscript = improvedTranscript.replaceAll(
+        `${bookName} ${chapter} ${verseNumberStartSpelledOut}`,
+        `${bookName} ${chapter}:${verseNumberStart}`,
+      );
+    }
+  }
+
   return improvedTranscript;
+}
+
+export function parseVerseReferenceIntoParts(verseReference: string) {
+  let bookNumber;
+  let verseReferenceWithoutBookNumber = verseReference;
+
+  if (Object.keys(["1", "2", "3"]).includes(verseReference.charAt(0))) {
+    verseReferenceWithoutBookNumber = verseReference
+      .split(verseReference.charAt(0))[1]
+      .trim();
+    bookNumber = Number(verseReference.charAt(0));
+  }
+
+  const bookNameRegExpMatchArray =
+    verseReferenceWithoutBookNumber.match(/[a-zA-Z]+/);
+  if (!bookNameRegExpMatchArray) {
+    throw new Error("Failed to parse book name out of verse reference");
+  }
+
+  const bookName = bookNameRegExpMatchArray[0];
+  const chapterAndVerses = verseReferenceWithoutBookNumber
+    .split(bookName)[1]
+    .trim();
+  const [chapter, verseResult] = chapterAndVerses.split(":");
+
+  const [verseNumberStart, verseNumberEnd] = verseResult.includes("-")
+    ? verseResult.split("-")
+    : [verseResult, verseResult];
+
+  return {
+    bookName,
+    bookNumber,
+    chapter: Number(chapter),
+    verseNumberStart: Number(verseNumberStart),
+    verseNumberEnd: Number(verseNumberEnd),
+  };
 }
