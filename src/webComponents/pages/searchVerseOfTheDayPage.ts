5bee784033e87cd084415a521ead9bdaf56aabef
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-09-15 10:22:15 -0500

    Add verse of the day page (#107)

---
+++ src/webComponents/pages/searchVerseOfTheDayPage.ts
@@ -0,0 +1,114 @@
+import { BasePage } from "./basePage";
+
+export class SearchVerseOfTheDayPage extends BasePage {
+  constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(this.#containerElement);
+    shadowRoot.appendChild(this.#styleElement);
+  }
+
+  static get observedAttributes() {
+    return [...BasePage.observedAttributes, "bible-id"];
+  }
+
+  get pageTitle() {
+    return "︎Verse of the Day | Memorize Bible Verses";
+  }
+
+  get bibleId() {
+    return this.getAttribute("bible-id");
+  }
+
+  get #bibleTranslationDropDownListElement() {
+    return this.shadowRoot!.querySelector(
+      "bible-translation-drop-down-list",
+    ) as HTMLElement;
+  }
+
+  get #bibleVerseOfTheDayFetchResultElement() {
+    return this.shadowRoot!.querySelector(
+      "bible-verse-of-the-day-fetch-result",
+    ) as HTMLElement;
+  }
+
+  get #containerElement() {
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `
+      <verse-text-page-template>
+        <span slot="page-heading">Search</span>
+
+        <span slot="page-description">
+          <p>Practice memorizing the verse of the day.</p>
+          <p>When you have it memorized go to Step 2.</p>
+        </span>
+
+        <span slot="page-content">
+          <bible-translation-drop-down-list></bible-translation-drop-down-list>
+          <bible-verse-of-the-day-fetch-result></bible-verse-of-the-day-fetch-result>
+        </span>
+
+        <span slot="page-navigation-back-button">&lt; Back</span>
+        <span slot="page-navigation-forward-button">Step 2 &gt;</span>
+      </verse-text-page-template>
+    `;
+
+    return divElement;
+  }
+
+  get #styleElement() {
+    const styleElement = document.createElement("style");
+    const css = `
+      p {
+        margin: 1rem 0;
+      }
+      bible-verse-of-the-day-fetch-result {
+        margin-top: 2rem;
+      }
+    `;
+    styleElement.textContent = css;
+    return styleElement;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.shadowRoot!.querySelector(
+      "verse-text-page-template",
+    )?.addEventListener("page-navigation-back-button-click", () =>
+      this.navigateToPage({ nextPage: "search-options-page" }),
+    );
+
+    this.shadowRoot!.querySelector(
+      "verse-text-page-template",
+    )?.addEventListener("page-navigation-forward-button-click", () =>
+      this.navigateToPage({
+        nextPage: "speak-page",
+        previousPage: "search-verse-of-the-day-page",
+      }),
+    );
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    super.attributeChangedCallback(name, oldValue, newValue);
+
+    for (const attributeName of SearchVerseOfTheDayPage.observedAttributes) {
+      if (name === attributeName) {
+        this.#bibleTranslationDropDownListElement?.setAttribute(
+          attributeName,
+          newValue,
+        );
+        this.#bibleVerseOfTheDayFetchResultElement?.setAttribute(
+          attributeName,
+          newValue,
+        );
+      }
+    }
+  }
+}
+
+window.customElements.define(
+  "search-verse-of-the-day-page",
+  SearchVerseOfTheDayPage,
+);
