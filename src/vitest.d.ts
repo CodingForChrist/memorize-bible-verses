5dc52a61fb689e3267fb5a11dbaecc236e0069f5
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-11-28 22:09:14 -0600

    run prettier

--- src/vitest.d.ts
+++ src/vitest.d.ts
@@ -2,13 +2,17 @@ declare module "corti" {
   class SpeechRecognition extends globalThis.SpeechRecognition {
     say(value: string | string[]): void;
   }
-  class SpeechRecognitionResultList extends globalThis.SpeechRecognitionResultList {
+  class SpeechRecognitionResultList
+    extends globalThis.SpeechRecognitionResultList
+  {
     constructor(results: SpeechRecognitionResult[]);
   }
   class SpeechRecognitionResult extends globalThis.SpeechRecognitionResult {
     constructor(alternatives: SpeechRecognitionAlternative[]);
   }
-  class SpeechRecognitionAlternative extends globalThis.SpeechRecognitionAlternative {
+  class SpeechRecognitionAlternative
+    extends globalThis.SpeechRecognitionAlternative
+  {
     constructor(transcript: string, confidence: number);
   }
 
