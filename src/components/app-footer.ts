b6337f944202a3dd512f9886920b34db23860c8f
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-12-13 10:38:54 -0600

    feat: add more content to app footer

--- src/components/app-footer.ts
+++ src/components/app-footer.ts
@@ -2,6 +2,7 @@ import { LitElement, css, html } from "lit";
 import { customElement } from "lit/decorators/custom-element.js";
 import { state } from "lit/decorators/state.js";
 
+import logoURL from "../images/logo.svg";
 import { hyperlinkStyles, buttonStyles } from "./shared-styles";
 
 @customElement("app-footer")
@@ -27,8 +28,34 @@ export class AppFooter extends LitElement {
       button.svg-icon-container:hover {
         color: var(--color-primary-mint-cream);
       }
-      p {
-        margin: 0 0 1rem 0;
+      .dialog-content {
+        text-align: center;
+      }
+      .dialog-content img {
+        width: 10rem;
+        margin-bottom: 2rem;
+      }
+      .dialog-content p {
+        margin: 0 0 2rem 0;
+        text-wrap: balance;
+      }
+      figure {
+        margin: 0 0 1rem;
+      }
+      .blockquote {
+        margin: 0 0 0.5rem 0;
+        font-size: 1.25rem;
+      }
+      .blockquote p {
+        margin: 0;
+      }
+      .blockquote-footer::before {
+        content: "— ";
+      }
+      .blockquote-footer {
+        margin-bottom: 2rem;
+        font-size: 0.875em;
+        color: #6c757d;
       }
     `,
   ];
@@ -63,16 +90,31 @@ export class AppFooter extends LitElement {
           this.isDialogOpen = false;
         }}
       >
-        <p>Memorize Bible Verses version ${this.version}</p>
-        <p>
-          This bible memorization app is free and open source.
-          <a
-            href="https://github.com/CodingForChrist/memorize-bible-verses"
-            target="_blank"
-            rel="noopener noreferrer"
-            >Check out the source code on GitHub</a
-          >.
-        </p>
+        <div class="dialog-content">
+          <img src=${logoURL} alt="Memorize Bible Verses" />
+
+          <figure>
+            <blockquote class="blockquote">
+              <p>
+                Your word I have hidden in my heart, that I might not sin
+                against You.
+              </p>
+            </blockquote>
+            <figcaption class="blockquote-footer">Psalm 119:11 NKJV</figcaption>
+          </figure>
+
+          <p>
+            This bible memorization app is a free tool for you to use to
+            practice memorizing scripture so you can grow in your faith.
+            <a
+              href="https://github.com/CodingForChrist/memorize-bible-verses"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Check out the source code on GitHub</a
+            >.
+          </p>
+          <p>app version ${this.version}</p>
+        </div>
       </modal-dialog>
       <footer>
         <button
