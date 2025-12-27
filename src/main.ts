5e1110d806fc322e1a4ebee9cb6439d5069d110d
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-12-26 19:10:21 -0600

    feat: host fonts using fontsource packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-import "./css/styles.css";
-import "./css/fonts.css";
+// JS
 import "./components";
 import "./pages";
+
+// CSS
+import "./css/styles.css";
+import "@fontsource/pt-sans/400.css";
+import "@fontsource/bebas-neue/400.css";
+// Noto Serif is used for scripture-styles
+import "@fontsource/noto-serif/400.css";
+import "@fontsource/noto-serif/400-italic.css";
+import "@fontsource/noto-serif/700.css";
+import "@fontsource/noto-serif/700-italic.css";
