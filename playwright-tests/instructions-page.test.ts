23f8dc1a0a8668ae08aa32180c0eda63591e353a
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2026-01-06 19:02:25 -0600

    fix: update url when falling back to instructions page

--- playwright-tests/instructions-page.test.ts
+++ playwright-tests/instructions-page.test.ts
@@ -1,11 +1,22 @@
 import { test, expect } from "@playwright/test";
 
 test("page load", async ({ page }) => {
-  await page.goto("");
-  await expect(page).toHaveTitle(/Instructions | Memorize Bible Verses/);
-
   await page.goto("/#/instructions");
   await expect(page).toHaveTitle(/Instructions | Memorize Bible Verses"/);
+});
+
+test("redirect to instructions page for any unknown url", async ({ page }) => {
+  const pageNames = [
+    "", // no hash
+    "/#/fake-page", // unknown page name
+    "/#/instrctions", // misspelled page name
+  ];
+
+  for (const pageName of pageNames) {
+    await page.goto(pageName);
+    await expect(page.url()).toContain("/#/instructions");
+    await expect(page).toHaveTitle(/Instructions | Memorize Bible Verses/);
+  }
 });
 
 test("get started button", async ({ page }) => {
