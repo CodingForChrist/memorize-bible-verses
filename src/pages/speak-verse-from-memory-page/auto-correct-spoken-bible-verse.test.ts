3e763520b6a60570f7fc8ea95965b165d197650c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2026-02-21 11:58:17 -0600

    WIP

--- src/pages/speak-verse-from-memory-page/auto-correct-spoken-bible-verse.test.ts
+++ src/pages/speak-verse-from-memory-page/auto-correct-spoken-bible-verse.test.ts
@@ -4,48 +4,57 @@ import { autoCorrectSpeechRecognitionInput } from "./auto-correct-spoken-bible-v
 
 describe("autoCorrectSpeechRecognitionInput()", () => {
   test("should add missing colon between chapter and verse number for a single verse", () => {
-    const transcript =
-      "Genesis 11 In the beginning God created the heavens and the earth Genesis 11";
-    const expectedOutput =
+    const verseTextGenesisChapter1Verse1 =
       "Genesis 1:1 In the beginning God created the heavens and the earth Genesis 1:1";
 
     expect(
       autoCorrectSpeechRecognitionInput({
-        transcript,
         verseReference: "Genesis 1:1",
-        verseText: expectedOutput,
+        transcript:
+          "Genesis 11 In the beginning God created the heavens and the earth Genesis 11",
+        verseText: verseTextGenesisChapter1Verse1,
       }),
-    ).toBe(expectedOutput);
+    ).toBe(verseTextGenesisChapter1Verse1);
   });
 
   test("should add missing colon between chapter and verse number for a range of verses", () => {
-    const transcript =
-      "Galatians 522 to 23 But the fruit of the Spirit is love joy peace patience kindness goodness faithfulness gentleness and self control against such things there is no law Galatians 522 to 23";
-    const expectedOutput =
+    const verseTextGalatiansChapter5Verses22To23 =
       "Galatians 5:22-23 But the fruit of the Spirit is love joy peace patience kindness goodness faithfulness gentleness and self control against such things there is no law Galatians 5:22-23";
 
     expect(
       autoCorrectSpeechRecognitionInput({
-        transcript,
         verseReference: "Galatians 5:22-23",
-        verseText: expectedOutput,
+        transcript:
+          "Galatians 522 to 23 But the fruit of the Spirit is love joy peace patience kindness goodness faithfulness gentleness and self control against such things there is no law Galatians 522 to 23",
+        verseText: verseTextGalatiansChapter5Verses22To23,
       }),
-    ).toBe(expectedOutput);
+    ).toBe(verseTextGalatiansChapter5Verses22To23);
   });
 
   test("should convert spelled out numbers to actual numbers", () => {
-    const transcript =
-      "Genesis one one In the beginning God created the heavens and the earth Genesis one one";
-    const expectedOutput =
+    const verseTextGenesisChapter1Verse1 =
       "Genesis 1:1 In the beginning God created the heavens and the earth Genesis 1:1";
 
     expect(
       autoCorrectSpeechRecognitionInput({
-        transcript,
         verseReference: "Genesis 1:1",
-        verseText: expectedOutput,
+        transcript:
+          "Genesis one one In the beginning God created the heavens and the earth Genesis one one",
+        verseText: verseTextGenesisChapter1Verse1,
       }),
-    ).toBe(expectedOutput);
+    ).toBe(verseTextGenesisChapter1Verse1);
+
+    const verseTextPsalmChapter23Verses1To2 =
+      "Psalm 23:1-2 The Lord is my shepherd I shall not want He makes me lie down in green pastures He leads me beside quiet waters Psalm 23:1-2";
+
+    expect(
+      autoCorrectSpeechRecognitionInput({
+        verseReference: "Psalms 23:1-2",
+        transcript:
+          "Psalm 23:1 through two The Lord is my shepherd I shall not want He makes me lie down in green pastures He leads me beside quiet waters psalm 23 one through two",
+        verseText: verseTextPsalmChapter23Verses1To2,
+      }),
+    ).toBe(verseTextPsalmChapter23Verses1To2);
   });
 
   test("should match lowercase book names", () => {
